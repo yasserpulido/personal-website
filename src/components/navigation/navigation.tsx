e8ec571ec3d2746fe6bd9e970228c200c25e1d15
Yasser <pulidoyasser@gmail.com>
2024-03-18 19:30:20 -0300

    Se cambio el estilo del logo.

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -68,7 +68,7 @@ const Navigation = () => {
   return (
     <header className="flex items-center justify-between h-20">
       <p>
-        <NavLink to="/" className="font-bold font-mono text-xl text-blue-500">
+        <NavLink to="/" className="font-bold font-mono text-xl text-blue-500 logo-text">
           {"<yasserpulido />"}
         </NavLink>
       </p>
